649834318b2ab32f5b4eea3fbdcf62ff1df3ad21
Noverdi Ramadhan <noverdy123@gmail.com>
2023-05-23 23:11:43 +0700

    change function to arrow function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home (): React.ReactElement {
+const Home = (): React.ReactElement => {
   return (
     <main>
       <h1 className='text-3xl'>Qiran App</h1>
     </main>
   )
 }
+
+export default Home
